3ce8a24355411fe5a1f24c7c140633a6b9437476
guruttosekai2011 <guruttosekai2011@gmail.com>
2015-11-07 00:53:31 +0900

    add Tree2 and modify

--- Python/2-GraphEssentials/ch2_2_Depth_First_Search.py
+++ Python/2-GraphEssentials/ch2_2_Depth_First_Search.py
@@ -4,15 +4,31 @@
 S_Node = "v1"
 
 
-def get_exampletree():
+"""
+sample network
+--Tree--
 
-    """
+       v1
+       |
+   v2-----v3
+   |       |
+v4---v5 v6---v7
+
+
+--Tree2--
+
            v1
-           |
-       v2-----v3
-       |       |
-    v4---v5 v6---v7
-    """
+            |
+     v2----v3----v4
+      |           |
+   v5---v6     v7---v8
+    |           |
+ v9---v10   v11---v12
+
+"""
+
+
+def get_exampletree():
 
     Tree = {
         "v1" : ["v2", "v3"],    
@@ -22,9 +38,27 @@ def get_exampletree():
         "v5" : [],    
         "v6" : [],    
         "v7" : [] 
-    }
+        }
 
-    return Tree
+
+    Tree2 = {
+        "v1" : ["v2", "v3", "v4"],
+        "v2" : ["v5", "v6"],
+        "v3" : [],
+        "v4" : ["v7", "v8"],
+        "v5" : ["v9", "v10"],
+        "v6" : [],
+        "v7" : ["v11", "v12"],
+        "v8" : [],
+        "v9" : [],
+        "v10": [],
+        "v11": [],
+        "v12": []
+        }
+
+
+    return Tree, Tree2
+
 
 
 def DFS(tree):
@@ -36,15 +70,16 @@ def DFS(tree):
         node = stack.pop()
         if node not in Order:
             Order.append(node)
-            New_node = set(Tree[node]) - set(Order)
+            New_node = set(tree[node]) - set(Order)
             stack.extend(New_node)
             
     return Order 
 
 if __name__ == '__main__':
 
-    Tree = get_exampletree()
-    Search_order = DFS(Tree)    
+    Tree, Tree2 = get_exampletree()
+    #Search_order = DFS(Tree)    
+    Search_order = DFS(Tree2)    
     print Search_order
 
 
